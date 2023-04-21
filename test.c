45541df2763c12625b7093a62b89502e90b9abd1
Het <62541263+HETFADIA@users.noreply.github.com>
2023-04-21 13:03:20 +0530

    erase stats

--- test.c
+++ test.c
@@ -259,9 +259,10 @@ int try_insert_request(struct ssd_info *ssd, struct request *req)
     // Calculate average read and write request size
     if (req->operation == READ)
         ssd->ave_read_size=(ssd->ave_read_size*ssd->read_request_count+req->size)/(ssd->read_request_count+1);
-    else
+    else if(req->operation == WRITE)
         ssd->ave_write_size=(ssd->ave_write_size*ssd->write_request_count+req->size)/(ssd->write_request_count+1);
-
+    else if (req->operation == ERASE) 
+        ssd->ave_erase_size=(ssd->ave_erase_size*ssd->erase_request_count+req->size)/(ssd->erase_request_count+1);
     if (req->next_node != NULL)
         ssd->next_request_time = req->next_node->time;
     else
@@ -790,11 +791,16 @@ int64_t trace_output(struct ssd_info* ssd){
                 ssd->read_request_count++;
                 ssd->read_avg=ssd->read_avg+(req->response_time-req->time);
             } 
-            else
+            else if(req->operation==WRITE)
             {
                 ssd->write_request_count++;
                 ssd->write_avg=ssd->write_avg+(req->response_time-req->time);
             }
+            else if (req->operation == ERASE) {
+                ssd->erase_request_count++;
+                ssd->erase_avg=ssd->erase_avg+(req->response_time-req->time);
+            } 
+                
 
             if(pre_node == NULL)
             {
@@ -883,11 +889,16 @@ int64_t trace_output(struct ssd_info* ssd){
                     ssd->read_request_count++;
                     ssd->read_avg=ssd->read_avg+(end_time-req->time);
                 } 
-                else
+                else if(req->operation==WRITE)
                 {
                     ssd->write_request_count++;
                     ssd->write_avg=ssd->write_avg+(end_time-req->time);
                 }
+                else if (req->operation == ERASE) 
+                {
+                    ssd->erase_request_count++;
+                    ssd->erase_avg=ssd->erase_avg+(end_time-req->time);
+                } 
 
                 while(req->subs!=NULL)
                 {
