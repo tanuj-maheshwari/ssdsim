8664f41f0a1ac5c76100b46cab30923edce4b64a
Het <62541263+HETFADIA@users.noreply.github.com>
2023-05-06 18:01:24 +0530

    stat file update

--- ssd.c
+++ ssd.c
@@ -1267,6 +1267,7 @@ void statistic_output(struct ssd_info *ssd)
     fprintf(ssd->statisticfile, "erase request count: %13u\n", ssd->erase_request_count);
     fprintf(ssd->statisticfile, "read request average size: %13f\n", ssd->ave_read_size);
     fprintf(ssd->statisticfile, "write request average size: %13f\n", ssd->ave_write_size);
+    fprintf(ssd->statisticfile, "erase request average size: %13f\n", ssd->ave_erase_size);
     if (ssd->read_request_count != 0)
         fprintf(ssd->statisticfile, "read request average response time: %lld\n", ssd->read_avg / ssd->read_request_count);
     if (ssd->write_request_count != 0)
@@ -1277,14 +1278,24 @@ void statistic_output(struct ssd_info *ssd)
     fprintf(ssd->statisticfile, "buffer read miss: %13lu\n", ssd->dram->buffer->read_miss_hit);
     fprintf(ssd->statisticfile, "buffer write hits: %13lu\n", ssd->dram->buffer->write_hit);
     fprintf(ssd->statisticfile, "buffer write miss: %13lu\n", ssd->dram->buffer->write_miss_hit);
+    /* fprintf(ssd->statisticfile, "buffer erase hits: %13lu\n", ssd->dram->buffer->erase_hits);
+    fprintf(ssd->statisticfile, "buffer erase miss: %13lu\n", ssd->dram->buffer->erase_miss_hit); */
     fprintf(ssd->statisticfile, "erase: %13u\n", erase);
     fprintf(ssd->statisticfile, "write sub request count: %13u\n", ssd->write_subreq_count);
     fprintf(ssd->statisticfile, "read subr request count: %13u\n", ssd->read_subreq_count);
     fprintf(ssd->statisticfile, "erase subr request count: %13u\n", ssd->erase_subreq_count);
     if (ssd->write_request_count != 0)
         fprintf(ssd->statisticfile, "write amplification: %.2f\n", (double)ssd->program_count / (double)ssd->write_subreq_count);
+    else
+        fprintf(ssd->statisticfile, "Write request count is 0");
     if (ssd->read_request_count != 0)
         fprintf(ssd->statisticfile, "read amplification: %.2f\n", (double)ssd->read_count / (double)ssd->read_subreq_count);
+    else
+        fprintf(ssd->statisticfile, "Read request count is 0");
+    if (ssd->erase_request_count!=0)
+        fprintf(ssd->statisticfile, "erase amplification: %.2f\n", (double)ssd->program_count / (double)ssd->erase_subreq_count);
+    else
+        fprintf(ssd->statisticfile, "Erase request count is 0");
     fprintf(ssd->statisticfile, "write amplification (size): %.2f\n", (double)ssd->in_program_size / (double)ssd->write_request_size);
     fprintf(ssd->statisticfile, "read amplification (size): %.2f\n", (double)ssd->in_read_size / (double)ssd->read_request_size);
     fprintf(ssd->statisticfile, "avg. gc page move: %.2f (%.2f%%)\n", (double)ssd->gc_move_page / (double)ssd->num_gc, (100 * ((double)ssd->gc_move_page / (double)ssd->num_gc) / ssd->parameter->page_block));
@@ -1292,6 +1303,7 @@ void statistic_output(struct ssd_info *ssd)
     fprintf(ssd->statisticfile, "\n\n pre-process duration: %lld ns\n", ssd->pre_process_time);
     fprintf(ssd->statisticfile, " simulation duration: %lld ns\n", ssd->simulation_end_time - ssd->simulation_start_time);
     fprintf(ssd->statisticfile, " IOPS: %.3f\n", (double)(ssd->read_count + ssd->program_count) / ((double)(ssd->simulation_end_time - ssd->simulation_start_time) / 1000000000));
+
     fprintf(ssd->statisticfile, " read BW: %.3f MB/s\n", ((double)ssd->read_request_size / 2000.0) / ((double)(ssd->simulation_end_time - ssd->simulation_start_time) / 1000000000));
     fprintf(ssd->statisticfile, " write BW: %.3f MB/s\n", ((double)ssd->write_request_size / 2000.0) / ((double)(ssd->simulation_end_time - ssd->simulation_start_time) / 1000000000));
     fprintf(ssd->statisticfile, " erase BW: %.3f MB/s\n", ((double)ssd->erase_request_size / 2000.0) / ((double)(ssd->simulation_end_time - ssd->simulation_start_time) / 1000000000));
@@ -1300,9 +1312,12 @@ void statistic_output(struct ssd_info *ssd)
     printf(" pre-process duration: %lld ns\n", ssd->pre_process_time);
     printf(" simulation duration: %lld ns\n", ssd->simulation_end_time - ssd->simulation_start_time);
     printf(" IOPS: %.3f\n", (double)(ssd->read_count + ssd->program_count) / ((double)(ssd->simulation_end_time - ssd->simulation_start_time) / 1000000000));
+
     printf(" read BW: %.3f MB/s\n", ((double)ssd->read_request_size / 2000.0) / ((double)(ssd->simulation_end_time - ssd->simulation_start_time) / 1000000000));
     printf(" write BW: %.3f MB/s\n", ((double)ssd->write_request_size / 2000.0) / ((double)(ssd->simulation_end_time - ssd->simulation_start_time) / 1000000000));
     printf(" erase BW: %.3f MB/s\n", ((double)ssd->erase_request_size / 2000.0) / ((double)(ssd->simulation_end_time - ssd->simulation_start_time) / 1000000000));
+
+    printf(" Queue length %llu\n",ssd->request_queue_length);
 }
 
 /***********************************************************************************
